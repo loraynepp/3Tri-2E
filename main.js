662680ef3405bb31f60d2deaa095626628dace2f
loraynepp <161638357+loraynepp@users.noreply.github.com>
2024-10-29 08:52:28 -0300

    29/10

--- main.js
+++ main.js
@@ -2,9 +2,21 @@ function criaCartao(categoria,pergunta,resposta){
     let container = document.getElementById('container')
     let article = document.createElement('article')
     article.className = 'cartao'
-    article.innerHTML = 'isso é um teste'
+    article.innerHTML = `
+      <div class= "cartao-conteudo">
+      <h3> $(categoria) </h3>
+      <div class= "cartao-conteudo-pergunta>
+      <p> $(pergunta)  </p>
+      </div>
+      <div class= "cartao-conteudo-resposta">
+      <p> $(resposta) </p>
+      </div>
+      </div>
+    `
+
     container.appendChild(article)
 
 }
 
 criaCartao('Biologia','Qual a função da mitocondria?', 'Produção de Energia (ATP)')
+criaCartao('Matemática', 'Como é definido um grupo?', 'È um conjunto de uma operação onde esta é fechada no conjunto e tem alguma propriedades')
