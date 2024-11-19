9692d5c496b704c9a6b42bba47f09eefabeb7019
loraynepp <161638357+loraynepp@users.noreply.github.com>
2024-11-19 08:48:00 -0300

    19/11

--- main.js
+++ main.js
@@ -4,13 +4,13 @@ function criaCartao(categoria,pergunta,resposta){
     article.className = 'cartao'
     article.innerHTML = `
       <div class= "cartao-conteudo">
-      <h3> $(categoria)</h3>
-      <div class= "cartao-conteudo-pergunta>
-      <p> $(pergunta)</p>
-      </div>
-      <div class= "cartao-conteudo-resposta">
-      <p> $(resposta)</p>
-      </div>
+        <h3> ${categoria}</h3>
+        <div class="cartao-conteudo-pergunta">
+          <p> ${pergunta}</p>
+        </div>
+        <div class="cartao-conteudo-resposta">
+          <p> ${resposta}</p>
+        </div>
       </div>
     `
 
